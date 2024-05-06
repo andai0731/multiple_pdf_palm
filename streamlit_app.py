a0f8c523bd00b97d8bad186d520e0c213de5ecd7
andai0731 <147966004+andai0731@users.noreply.github.com>
2024-05-06 17:21:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,6 @@ from dotenv import load_dotenv
 #import pinecone
 from pinecone import Pinecone
 
-pc = Pinecone(api_key="081c6b89-ff28-4673-9a4b-5912b5cfcff3")
-index = pc.Index("llm")
-
 # Load environment variables from .env file
 load_dotenv()
 
@@ -38,8 +35,11 @@ def get_text_chunks(text):
 def get_vector_store(text_chunks):
     embeddings = GooglePalmEmbeddings()
     # Initialize Pinecone
-    #Pinecone.init(api_key=PINECONE)  # Use your Pinecone API key
-    pc.init(api_key=PINECONE)
+    #Pinecone(api_key=PINECONE)  # Use your Pinecone API key
+
+
+    pc = Pinecone(api_key="081c6b89-ff28-4673-9a4b-5912b5cfcff3")
+    index = pc.Index("llm")
     #vector_store = pinecone.Index("llm")
     vector_store = index
     vector_store.upsert(items=text_chunks, ids=range(len(text_chunks)))
