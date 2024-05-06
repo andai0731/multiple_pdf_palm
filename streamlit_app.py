b5b8cdf502ed0e51d3c72a177d1c4f120e442a52
andai0731 <147966004+andai0731@users.noreply.github.com>
2024-05-06 16:51:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,18 @@ from langchain.memory import ConversationBufferMemory
 import os
 
 from dotenv import load_dotenv
-API = os.getenv("GOOGLE_API_KEY")
+import pinecone
+
 # Load environment variables from .env file
 load_dotenv()
 
-#os.environ['GOOGLE_API_KEY'] =  'AIzaSyAWtXHiIVP3GYwmwf4rOEsJKXKtPCsDUkc'
-
 def get_pdf_text(pdf_docs):
-    text=""
+    text = ""
     for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
+        pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+            text += page.extract_text()
+    return text
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
@@ -32,25 +31,27 @@ def get_text_chunks(text):
 
 def get_vector_store(text_chunks):
     embeddings = GooglePalmEmbeddings()
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    # Initialize Pinecone
+    pinecone.init(api_key=os.getenv("PINECONE_API_KEY"))  # Use your Pinecone API key
+    vector_store = pinecone.Index("my-index")
+    vector_store.upsert(items=text_chunks, ids=range(len(text_chunks)))
     return vector_store
 
 def get_conversational_chain(vector_store):
-    #llm=GooglePalm()
     llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=API, temperature=0.1)
-    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
-    #conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever())
     return conversation_chain
 
 def user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chatHistory = response['chat_history']
     for i, message in enumerate(st.session_state.chatHistory):
-        if i%2 == 0:
+        if i % 2 == 0:
             st.write("Human: ", message.content)
         else:
             st.write("Bot: ", message.content)
+
 def main():
     st.set_page_config("Chat with Multiple PDFs")
     st.header("Chat with Multiple PDF 💬")
@@ -72,8 +73,6 @@ def main():
                 vector_store = get_vector_store(text_chunks)
                 st.session_state.conversation = get_conversational_chain(vector_store)
                 st.success("Done")
-
-
 
 if __name__ == "__main__":
     main()
